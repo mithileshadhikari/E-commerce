1e33b1634b5ce801b179c586814576878f6b7f08
mithileshadhikari <133491722+mithileshadhikari@users.noreply.github.com>
2023-06-21 19:18:57 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function add_to_cart(pid,pname,price)
+{
+	let cart=localStorage.getItem("cart");
+	if(cart==null)
+	{
+		//no cart
+		let products=[];
+		let product={productId:pid,productName:pname,productQuantity:1,productPrice:price}
+		products.push(product);
+		localStorage.setItem("cart",JSON.stringify(products));
+	}
+	else
+	{
+		//cart is present
+	}
+}
